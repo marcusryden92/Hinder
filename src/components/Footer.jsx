d8b400ccef7c7857323f6af50e21df3ade1a5fb3
marcusryden92 <marcus.ryden1992@gmail.com>
2024-01-29 16:02:07 +0100

    Footer, Camera

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
-function Footer() {
-  return <div className={`absolute bottom-0 bg-red-400 w-screen`}>Footer</div>;
-}
+import { useState, useEffect } from "react";
 
-export default Footer;
+export default function Footer() {
+  const [quote, setQuote] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      fetch("https://type.fit/api/quotes")
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          const randomQuote = Math.floor(Math.random() * data.length);
+          const newQuote = data[randomQuote].text;
+          if (newQuote.length > 70) {
+            fetchData();
+          } else {
+            setQuote(newQuote);
+          }
+        });
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div
+      className={`absolute bottom-0 w-screen font-serif
+    align-middle
+    text-center
+    italic
+    text-sm
+    w-screen 
+    p-2 h-14
+    pt-4 
+    shadow-lg
+    text-gray-300
+    bg-zinc-900 `}
+    >
+      "{quote}"
+    </div>
+  );
+}
